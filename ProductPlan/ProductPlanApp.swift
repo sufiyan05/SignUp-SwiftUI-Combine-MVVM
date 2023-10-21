54682c1489041a23f152bbd1e140525de54b8895
sufiyan05 <sufiyansitponia60@gmail.com>
2023-10-21 12:31:36 +0530

    Initial Commit

---
+++ ProductPlan/ProductPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductPlanApp.swift
+//  ProductPlan
+//
+//  Created by Sufiyan Sitponiya on 21/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ProductPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
